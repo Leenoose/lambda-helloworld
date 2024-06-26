5c2c10e8dfb724382c42409f0a58072e4cc7d25e
Linus <elfzity@hotmail.com>
2024-06-26 17:28:48 +0800

    Added project for REST with no Lambda dependency

---
+++ RESTHelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace RESTHelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost(Name = "JsonToUpper")]
+
+        public string FunctionHandler(JsonObject input)
+        {
+            return input.ToString().ToUpper();
+        }
+    }
+}
